c8f14027d4c9cda833737165e6d7627e7e428ba5
vc743 <victorcontrerasvargas55@gmail.com>
2024-09-03 20:05:45 -0400

    First commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { AddCategory, GifGrid } from "./components";
+
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["One Punch"]);
+
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
+
+    setCategories([newCategory, ...categories]);
+  };
+
+  return (
+    <>
+      {/* Titulo */}
+      <h1>GifExpertApp</h1>
+
+      {/* Input */}
+      <AddCategory onNewCategory={(value) => onAddCategory(value)} />
+
+      {/* Listado de Gif */}
+      <ol>
+        {categories.map((category) => (
+          <GifGrid key={category} category={category} />
+        ))}
+      </ol>
+    </>
+  );
+};
